6947884b74f0e2db5adba888305d4e1fa4d34e0f
Alex Quang <alexquang1991@gmail.com>
2016-05-04 10:49:04 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, './client/static')));
+
+app.listen(8000, function() {
+	console.log('Server is listening on port 8000');
+});
