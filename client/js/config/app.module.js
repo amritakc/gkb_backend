7176786c0afc6e7b8ce90401ff3846c0ee64de53
Alex Quang <alexquang1991@gmail.com>
2016-05-04 12:00:46 -0700

    made initial angular module

---
+++ client/js/config/app.module.js
@@ -0,0 +1,24 @@
+var myApp = app.module('myApp', ['ngRoute']);
+	
+	myApp.config(function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'partials/main.html',
+				controller: 'mainCtrl'
+			})
+			.when('/volunteers', {
+				templateUrl: 'partials/volunteer.html',
+				controller: 'volunteersController'
+			})
+			.when('/donations', {
+				templateUrl: 'partials/donate.html',
+				controller: 'donationsController'
+			})
+			.when('/shop', {
+				templateUrl: 'partials/shop.html',
+				controller: 'shopCtrl'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
