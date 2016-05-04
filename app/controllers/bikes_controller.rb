60f31c6ca8b5a197b2432e80945eb46ea6a1e136
CC Wang <chongchong1110@gmail.com>
2016-05-04 14:52:26 -0700

    type not working Invalid single-table inheritance type: Kid Bike is not a subclass of Bike

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -6,14 +6,14 @@ class BikesController < ApplicationController
   end
 
   def create
-    bike = Bike.new(donor:params[:donor],type:params[:type],brand:params[:brand],value:params[:value],date_sold:[params:date_sold],barcode:[params:barcode],ebay:[params:ebay])
+    bike = Bike.new(donor:params[:donor],type:params[:type_bike],brand:params[:brand],value:params[:value],barcode:params[:barcode],ebay:params[:ebay])
     if bike.save
 
     else
       flash[:errors] = bike.errors.full_messages
     end
 
-    redirect_to :back  
+    redirect_to :back
   end
 
   def show
