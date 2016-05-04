1d524d664eca2e25853042f5ee19fb65ec9ccf7e
Antonisfk <antonisfkiaras@gmail.com>
2016-05-04 14:37:15 -0700

    Fixed folder structure to componets

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// require express so that we can build an express app
+var express = require('express');
+// require path so that we can use path stuff like path.join
+var path = require('path');
+var bodyParser = require('body-parser');
+// instantiate the app
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// set up a static file server that points to the "client" directory
+app.use(express.static(path.join(__dirname, '../client/src/app')));
+app.listen(8000, function() {
+  console.log('cool stuff on: 8000');
+});
