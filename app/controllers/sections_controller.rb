3fd956b7e7ae42eea83c939529cdb4381f5ca21c
v3k <ktw777@yahoo.com>
2016-05-10 17:21:05 -0700

    changed methods for section

---
+++ app/controllers/sections_controller.rb
@@ -0,0 +1,29 @@
+class SectionsController < ApplicationController
+  def index
+  	
+
+  end
+
+  def show
+  	@sec = Section.find_by_name(params[:section]).contents
+    render :json => @sec@inform
+
+  end
+
+  def edit
+  	
+  end
+
+  def update
+  	
+  end
+
+  def create
+
+
+  end
+
+  def new
+
+  end
+end
