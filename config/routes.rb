94fdc2329ae67c93a699c32bf01ea17bf4cbd174
v3k <ktw777@yahoo.com>
2016-05-17 16:30:20 -0700

    added documentation to routes.rb and refactored code

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,31 @@
 Rails.application.routes.draw do
 
   devise_for :users#, :controllers => {sessions: 'sessions'} 
-  # devise_for :admins
-  # root to: 'admins#index'
+  
   root to: 'application#angular'
 
+  # routes for section 
   get 'sections/:section' => 'sections#show'
 
-
+  # routes for contents
   get 'contents/index'
-
   get 'contents/show/:id' => 'contents#show'
-
-  get 'contents/edit/:id' => 'contents#edit'
-
-  get 'contents/new' => 'contents#new'
-
+  # get 'contents/edit/:id' => 'contents#edit'   # used for testing 
+  # get 'contents/new' => 'contents#new'          # used for testing
   post 'contents/create' => 'contents#create'
-  
-
   patch 'contents/update/:id' => 'contents#update'
-
-
   delete 'contents/destroy/:id' => 'contents#destroy'
+  
+  # routes for bikes inventory "FOR FUTURE IMPLEMENTATION"
+  # get 'bikes/index'
+  # get 'bikes/new'
+  # get 'bikes/show/:id' =>'bikes#show'
+  # get 'bikes/edit/:id' => 'bikes#edit'
+  # post 'bikes/create' =>'bikes#create'
+  # post 'bikes/update/:id' => 'bikes#update'
+  # delete 'bikes/destroy/:id' => 'bikes#destroy'
 
-  get 'contents/section/:section' => 'contents#section'
-
-
-
-  get 'bikes/index'
-
-  get 'bikes/new'
-
-  post 'bikes/create' =>'bikes#create'
-
-  get 'bikes/show/:id' =>'bikes#show'
-
-  get 'bikes/edit/:id' => 'bikes#edit'
-
-  post 'bikes/update/:id' => 'bikes#update'
-
-  delete 'bikes/destroy/:id' => 'bikes#destroy'
-
+  # route for redirecting user to the home landing page if request is not found
   get '*path' => redirect('/#/home')
   
 end
