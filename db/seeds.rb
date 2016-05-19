c8c21661c107c3b76b20157b37c2f509179f3b68
mml1 <mmlliguicota@gmail.com>
2016-05-19 14:10:33 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,19 @@
 User.create(:email => 'user@gmail.com', :password => 'password', :password_confirmation => 'password')
 
 
-# default values for Section
+# Default values for Section
 Section.create(:name => 'announcements')
 Section.create(:name => 'news')
 Section.create(:name => 'bikes')
+Section.create(:name => 'programs')
 
-
+# Starting content for site
+Content.create(:title =>"Raleigh Lightweight", :color=> "Purple", :brand=>"Road Bike", :price=>300.00, :section=>Section.find_by_name("bikes"))
+Content.create(:title =>"Raleigh Lightweight", :color=> "Purple", :brand=>"Road Bike", :price=>300.00, :section=>Section.find_by_name("bikes"))
+Content.create(:title =>"Mongoose", :color=> "Navy Blue", :brand=>"Mountain Bike", :price=>100.00, :section=>Section.find_by_name("bikes"))
+Content.create(:title =>"Repco Challenger", :color=> "Blue and Orange", :brand=>"Mountain Bike", :price=>110.00, :section=>Section.find_by_name("bikes"))
+Content.create(:title =>"Funky Bike", :color=> "Zebra Stripe", :brand=>"RoadBike", :price=>300.00, :section=>Section.find_by_name("bikes"))
+Content.create(:title =>"Abeni 10 Speed", :color=> "Yellow", :brand=>"RoadBike", :price=>210.00, :section=>Section.find_by_name("bikes"))
+Content.create(:title =>"Yellow Bike", :color=> "Yellow", :brand=>"Bicycle", :price=>200.00, :section=>Section.find_by_name("bikes"))
+Content.create(:title =>"Morrison Freeway", :color=> "Blue", :brand=>"RoadBike", :price=>170.00, :section=>Section.find_by_name("bikes"))
+Content.create(:title =>"Raleigh Lightweight", :color=> "Purple", :brand=>"Road Bike", :price=>300.00, :section=>Section.find_by_name("bikes"))
