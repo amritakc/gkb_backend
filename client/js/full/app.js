7f82898aec5be349d8c9553c5decb140cee614f0
Alex Quang <alexquang1991@gmail.com>
2016-05-20 14:27:48 -0700

    carousel is done

--- client/js/full/app.js
+++ client/js/full/app.js
@@ -58,6 +58,28 @@ function picFactory() {
 
 	return factory;
 }
+myApp.animation('.slide-animation', function() {
+	return {
+		addClass: function (element, className, done) {
+			if (className == 'ng-hide') {
+				TweenMax.to(element, 0.5, {left: -element.parent().width(), onComplete: done });
+			}
+			else {
+				done();
+			}
+		},
+		removeClass: function (element, className, done) {
+			if (className == 'ng-hide') {
+				element.removeClass('ng-hide');
+				TweenMax.set(element, { left: element.parent().width() });
+                TweenMax.to(element, 0.5, {left: 0, onComplete: done });
+			}
+			else {
+				done();
+			}
+		}
+	};
+});
 myApp.controller('communityCtrl', communityCtrl);
 
 function communityCtrl($scope) {
