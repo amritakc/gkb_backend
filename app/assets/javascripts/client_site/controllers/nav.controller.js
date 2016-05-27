78856d6b4513daf21a9b9b964249c56e7c6d1263
Alex Quang <alexquang1991@gmail.com>
2016-05-26 19:35:17 -0700

    working with carousel

--- app/assets/javascripts/client_site/controllers/nav.controller.js
+++ app/assets/javascripts/client_site/controllers/nav.controller.js
@@ -1,11 +1,20 @@
-myApp.controller('navCtrl', function($rootScope, $scope, $location, $window, locFactory) {
+myApp.controller('navCtrl', function($rootScope, $scope, $location, $window, locFactory, slideFactory) {
 	$scope.small = false;
 	$scope.showLinks = showLinks;
 	$scope.changePage = changePage;
+	$scope.myInterval = 5000;
+	$scope.active = 0;
+	$scope.noWrapSlides = false;
+	$scope.slides = [];
 
 	// Scrolls page to top on route change
 	$rootScope.$on('$routeChangeSuccess', function() {
 		document.body.scrollTop = document.documentElement.scrollTop = 0;
+    });
+
+    // Gets the slides
+    slideFactory.getSlides(function(res) {
+    	$scope.slides = res;
     });
 
 	function changePage(place) {
--- app/assets/javascripts/client_site/models/newFactory.js
+++ app/assets/javascripts/client_site/models/newFactory.js
@@ -3,7 +3,7 @@ myApp.factory('newFactory', newFactory);
 // News functions to retrieve information
 function newFactory($http) {
 	var factory = {};
-	var newsContent =[]
+	var newsContent =[];
 
 	factory.news = function(callback){
 		$http.get('sections/news').success(function(cInfo){
